8ffb9a0173ea050058e9c0f4e343da2f5fd5b71f
sarang salame <115799065+sarangsalame@users.noreply.github.com>
2023-01-08 13:37:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React,{useReducer} from 'react'
 import '../styles/App.css';
 const App = () => {
+  let taskState={
+  count:0
+  }
+  function reducer(state, action){
+    switch(action.type){
+      case 'add':
+        return {taskState.count:state.count+1}
+      case 'delete':
+        return {taskState.count:state.count+1}
+      case 'deleteAll':
+        return {taskState.count:0}
+    }
+  }
 
   return (
+    const [state, dispatch] = useReducer(reducer, taskState)
     <div id="main">
       <h2>Task Counter</h2>
       <button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
